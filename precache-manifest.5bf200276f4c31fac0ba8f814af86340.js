0d3c642cf4538f5a655e4da2e96d4878856bb50f
Yavuzgrdl <32365733+Yavuzgrdl@users.noreply.github.com>
2020-04-16 21:17:48 +0200

    Updates

---
+++ precache-manifest.5bf200276f4c31fac0ba8f814af86340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b24b6ca6fab4aeea5eff94154403f2f",
+    "url": "/json-server_and_staff-registration/index.html"
+  },
+  {
+    "revision": "1ac7c6ef74f093a5684f",
+    "url": "/json-server_and_staff-registration/static/css/main.d2be01b2.chunk.css"
+  },
+  {
+    "revision": "98f978d45aea180388ae",
+    "url": "/json-server_and_staff-registration/static/js/2.3b69a8e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/json-server_and_staff-registration/static/js/2.3b69a8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac7c6ef74f093a5684f",
+    "url": "/json-server_and_staff-registration/static/js/main.6b2a1229.chunk.js"
+  },
+  {
+    "revision": "0668c75082ed1ec8988a",
+    "url": "/json-server_and_staff-registration/static/js/runtime-main.47a12c0c.js"
+  }
+]);
